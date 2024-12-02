fda7cd96c444c25a0d8b0f8e91c65423ce4ca785
purelind <purelind@gmail.com>
2024-12-02 17:55:52 +0800

    chore update config token

--- internal/config/config.go
+++ internal/config/config.go
@@ -41,7 +41,7 @@ func Load() *Config {
     cfg.LogPath = getEnv("LOG_PATH", "logs/tiup_checker.log")
 
     // github token
-    cfg.GitHubToken = getEnv("GITHUB_TOKEN", "")
+    cfg.GitHubToken = getEnv("API_GITHUB_TOKEN", "")
 
     
     return cfg
